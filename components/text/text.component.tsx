392a0e44c34f87729253a8b3c351669e0edb1d71
Kagublanca <reecekagu@gmail.com>
2022-03-07 18:31:38 +0000

    Slightly working? It checks if the character is correct, and if you type backspace. Still some issues

--- components/text/text.component.tsx
+++ components/text/text.component.tsx
@@ -5,11 +5,32 @@ function Text({something} : TTextProps) {
 
 const initialState = "This is a paragraph that you will type out, or something!"
 const [text, setText] = useState(initialState);
+let number = 0;
+let previousNumber = Number;
+
+function onType(e : any){
+    if(previousNumber > e.target.value.length){
+        number--;
+        console.log("oOdksopk")
+    previousNumber = e.target.value.length;
+    }
+
+const userText = e.target.value;
+
+if(text[number] === userText[number]){
+    number++
+previousNumber = e.target.value.length;
+return console.log("Match!!");
+}
+previousNumber = e.target.value.length;
+return null;
+
+}
 
 return(
 <>
 <h1>{text}</h1>
-<input placeholder="This is where you type"></input>
+<input placeholder="This is where you type" onChange={onType}></input>
 </>
 )
 }
